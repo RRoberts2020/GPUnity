2fbaf7c62b0a3123dd749e000462f27b516f1794
Reece Roberts <Reece2.Roberts@live.uwe.ac.uk>
2022-03-25 15:33:53 +0000

    D5 and D6 started
    D5 - 2.5D Gameplay on a Spline (Started)
    
    D6 - Platform Sequence (Started)

---
+++ GP-Ds/Assets/D5/Spine.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+public class Spine : MonoBehaviour
+{
+    public GameObject FocusCam;
+    public GameObject targetOjbect;
+
+    public PathCreator pathCreator;
+    public EndOfPathInstruction end; // What will happen if the end of path is reached
+    public float speed; // Speed of game object
+    float dstTravelled;
+    void Update()
+    {
+        dstTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(dstTravelled, end);
+
+        FocusCam.transform.LookAt(targetOjbect.transform);
+    }
+}
