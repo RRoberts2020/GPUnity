e7254eba293907d285a72a1863429bbfcd73f05e
Reece Roberts <Reece2.Roberts@live.uwe.ac.uk>
2022-03-28 16:32:06 +0100

    D5 and D6 Minor bug fixes
    Had to quickly fix error that was interfering with the D3 camera switch states, and the Onenter and Onexit functions for the camera spline

--- GP-Ds/Assets/D5/SplineCamStates.cs
+++ GP-Ds/Assets/D5/SplineCamStates.cs
@@ -7,36 +7,12 @@ public class SplineCamStates : MonoBehaviour
     public GameObject splineCam;
     public GameObject normalCam;
 
-    public bool enterTrigger;
-
-
-    void Start()
-    {
-        enterTrigger = false;
-
-    }
-
-    void Update()
-    {
-
-        if (enterTrigger == true)
-        {
-            splineCam.SetActive(true);
-            normalCam.SetActive(false);
-
-        }
-        else if (enterTrigger == false)
-        {
-            splineCam.SetActive(false);
-            normalCam.SetActive(true);
-        }
-    }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            enterTrigger = true;
+            splineCam.SetActive(true);
+            normalCam.SetActive(false);
         }
     }
 
@@ -44,7 +20,8 @@ public class SplineCamStates : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            enterTrigger = false;
+            splineCam.SetActive(false);
+            normalCam.SetActive(true);
         }
     }
 }
